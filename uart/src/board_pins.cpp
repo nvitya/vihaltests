458da0b2e491f9282e2e52ed7dde92be7f7dc8c5
nvitya <nvitya@gmail.com>
2021-10-30 18:30:18 +0200

    uart test changed to using board_pins.cpp + more boards added

---
+++ uart/src/board_pins.cpp
@@ -0,0 +1,317 @@
+/*
+ *  file:     board_pins.cpp (spiflash)
+ *  brief:    SPI Flash Test Board pins
+ *  version:  1.00
+ *  date:     2021-10-29
+ *  authors:  nvitya
+*/
+
+#include "board_pins.h"
+
+THwUart   conuart;  // console uart
+unsigned  pin_led_count = 1;
+
+TGpioPin  pin_led[MAX_LEDS] =
+{
+  TGpioPin(),
+  TGpioPin(),
+  TGpioPin(),
+  TGpioPin()
+};
+
+/* NOTE:
+     for direct GPIO pin definitions is simpler to define with port and pin number:
+
+       TGpioPin  pin_mygpio(PORTNUM_C, 13, false);
+
+     but don't forget to initialize it in the setup code:
+
+       pin_mygpio.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+*/
+
+void board_pins_init_leds()
+{
+  for (unsigned n = 0; n < pin_led_count; ++n)
+  {
+    pin_led[n].Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+  }
+}
+
+#if 0  // to use elif everywhere
+
+//-------------------------------------------------------------------------------
+// Risc-V (RV32I)
+//-------------------------------------------------------------------------------
+
+#elif defined(BOARD_LONGAN_NANO)
+
+void board_pins_init()
+{
+  pin_led_count = 3;
+  pin_led[0].Assign(PORTNUM_C, 13, true);
+  pin_led[1].Assign(PORTNUM_A,  1, true);
+  pin_led[2].Assign(PORTNUM_A,  2, true);
+  board_pins_init_leds();
+
+  hwpinctrl.PinSetup(PORTNUM_A,  9, PINCFG_OUTPUT | PINCFG_AF_0);
+  hwpinctrl.PinSetup(PORTNUM_A, 10, PINCFG_INPUT  | PINCFG_AF_0);
+  conuart.Init(0); // USART0
+}
+
+#elif defined(MCUF_VRV100)
+
+#define HEXNUM_DISPLAY
+void board_show_hexnum(unsigned ahexnum)
+{
+  volatile uint32_t *  hexnum = (volatile uint32_t *)0xF1000000;
+  *hexnum = ahexnum;
+}
+
+#if SPI_SELF_FLASHING
+
+  #include "hwspi.h"
+  #include "spiflash.h"
+
+  THwSpi     fl_spi;
+  TSpiFlash  spiflash;
+
+#endif
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_A, 0, false);
+  board_pins_init_leds();
+
+  board_show_hexnum(0);
+
+  conuart.Init(1); // UART1
+
+  #if SPI_SELF_FLASHING
+
+    fl_spi.speed = 10000000;
+    fl_spi.Init(1); // flash
+
+    spiflash.spi = &fl_spi;
+    spiflash.has4kerase = false; // warning some ECP devices does not have 4k erase !
+    spiflash.Init();
+
+  #endif
+}
+
+//-------------------------------------------------------------------------------
+// ARM Cortex-M
+//-------------------------------------------------------------------------------
+
+#elif defined(BOARD_MIN_F103)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_C, 13, false);
+  board_pins_init_leds();
+
+  // USART1
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_0);  // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_0 | PINCFG_PULLUP);  // USART1_RX
+  conuart.Init(1);
+}
+
+#elif defined(BOARD_MIN_F401) \
+      || defined(BOARD_MIBO48_STM32F303) \
+    || defined(BOARD_MIBO64_STM32F405) \
+    || defined(BOARD_MIBO48_STM32G473)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_C, 13, false);
+  board_pins_init_leds();
+
+  // USART1
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_7 | PINCFG_PULLUP);  // USART1_RX
+  conuart.Init(1);
+}
+
+
+#elif defined(BOARD_DISCOVERY_F072)
+
+TGpioPin  led1pin(PORTNUM_C, 6, false);
+TGpioPin  led2pin(PORTNUM_C, 8, false);
+TGpioPin  led3pin(PORTNUM_C, 9, false);
+TGpioPin  led4pin(PORTNUM_C, 7, false);
+
+void board_pins_init()
+{
+  pin_led_count = 4;
+  pin_led[0].Assign(PORTNUM_C, 6, false);
+  pin_led[1].Assign(PORTNUM_C, 8, false);
+  pin_led[2].Assign(PORTNUM_C, 9, false);
+  pin_led[3].Assign(PORTNUM_C, 7, false);
+  board_pins_init_leds();
+
+  // USART1 - not availabe on the embedded debug probe
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_1);  // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_1);  // USART1_RX
+  conuart.Init(1);
+}
+
+#elif defined(BOARD_MIBO20_STM32F030) || defined(BOARD_MIBO20_STM32F070)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_B, 1, false);
+  board_pins_init_leds();
+
+  // USART1
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_1);  // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_1 | PINCFG_PULLUP);  // USART1_RX
+  conuart.Init(1);
+}
+
+#elif defined(BOARD_NUCLEO_F446) || defined(BOARD_NUCLEO_F746) || defined(BOARD_NUCLEO_H743)
+
+void board_pins_init()
+{
+  pin_led_count = 3;
+  pin_led[0].Assign(PORTNUM_B,  0, false);
+  pin_led[1].Assign(PORTNUM_B,  7, false);
+  pin_led[2].Assign(PORTNUM_B, 14, false);
+  board_pins_init_leds();
+
+  // USART3: Stlink USB / Serial converter
+  hwpinctrl.PinSetup(PORTNUM_D, 8,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART3_TX: PD.8
+  hwpinctrl.PinSetup(PORTNUM_D, 9,  PINCFG_INPUT  | PINCFG_AF_7);  // USART3_RX: Pd.9
+  conuart.Init(3); // USART3
+}
+
+#elif defined(BOARD_NUCLEO_H723)
+
+void board_pins_init()
+{
+  pin_led_count = 3;
+  pin_led[0].Assign(PORTNUM_B,  0, false);  // PB0 or PA5
+  pin_led[1].Assign(PORTNUM_E,  1, false);
+  pin_led[2].Assign(PORTNUM_B, 14, false);
+  board_pins_init_leds();
+
+  // USART3: Stlink USB / Serial converter
+  hwpinctrl.PinSetup(PORTNUM_D, 8,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART3_TX: PD.8
+  hwpinctrl.PinSetup(PORTNUM_D, 9,  PINCFG_INPUT  | PINCFG_AF_7);  // USART3_RX: Pd.9
+  conuart.Init(3); // USART3
+}
+
+#elif defined(BOARD_ARDUINO_DUE)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_B, 27, false);
+  board_pins_init_leds();
+
+  // UART - On the Arduino programmer interface
+  hwpinctrl.PinSetup(0, 8, PINCFG_INPUT | PINCFG_AF_0);  // UART_RXD
+  hwpinctrl.PinSetup(0, 9, PINCFG_OUTPUT | PINCFG_AF_0); // UART_TXD
+  conuart.Init(0);  // UART
+}
+
+
+#elif defined(BOARD_XPLAINED_SAME70)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_C, 8, false);
+  board_pins_init_leds();
+
+  // USART1 - EDBG
+  hwpinctrl.PinSetup(0, 21, PINCFG_INPUT | PINCFG_AF_0);  // USART1_RXD
+  MATRIX->CCFG_SYSIO |= (1 << 4); // select PB4 instead of TDI !!!!!!!!!
+  hwpinctrl.PinSetup(1,  4, PINCFG_OUTPUT | PINCFG_AF_3); // USART1_TXD
+  conuart.Init(0x101); // USART1
+
+  // UART3 - Arduino shield
+  //hwpinctrl.PinSetup(3, 28, PINCFG_INPUT | PINCFG_AF_0);  // UART3_RXD
+  //hwpinctrl.PinSetup(3, 30, PINCFG_OUTPUT | PINCFG_AF_0); // UART3_TXD
+  //uartx2.Init(3); // UART3
+}
+
+#elif defined(BOARD_XPRESSO_LPC54608)
+
+void board_pins_init()
+{
+  pin_led_count = 3;
+  pin_led[0].Assign(2,  2, true);
+  pin_led[1].Assign(3,  3, true);
+  pin_led[2].Assign(3, 14, true);
+  board_pins_init_leds();
+
+  hwpinctrl.PinSetup(0, 30, PINCFG_OUTPUT | PINCFG_AF_1); // UART_TX:
+  hwpinctrl.PinSetup(0, 29, PINCFG_INPUT  | PINCFG_AF_1); // UART_RX:
+  conuart.Init(0);
+}
+
+#elif defined(BOARD_MIBO64_ATSAME5X)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_A, 1, false);
+  board_pins_init_leds();
+
+  // SERCOM0
+  hwpinctrl.PinSetup(PORTNUM_A, 4, PINCFG_OUTPUT | PINCFG_AF_3);  // PAD[0] = TX
+  hwpinctrl.PinSetup(PORTNUM_A, 5, PINCFG_INPUT  | PINCFG_AF_3);  // PAD[1] = RX
+  conuart.Init(0);
+
+  // SERCOM2
+  //hwpinctrl.PinSetup(PORTNUM_A, 12, PINCFG_AF_2);  // PAD[0] = TX
+  //hwpinctrl.PinSetup(PORTNUM_A, 13, PINCFG_AF_2);  // PAD[1] = RX
+  //conuart.Init(2);
+}
+
+#elif defined(BOARD_VERTIBO_A)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_A, 29, false);
+  board_pins_init_leds();
+
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_INPUT  | PINCFG_AF_0);  // UART0_RX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_OUTPUT | PINCFG_AF_0);  // UART0_TX
+  conuart.baudrate = 115200;
+  conuart.Init(0);
+}
+
+#elif defined(BOARD_ENEBO_A)
+
+void board_pins_init()
+{
+  pin_led_count = 3;
+  pin_led[0].Assign(PORTNUM_A, 20, true);
+  pin_led[1].Assign(PORTNUM_D, 14, true);
+  pin_led[2].Assign(PORTNUM_D, 13, true);
+  board_pins_init_leds();
+
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_INPUT  | PINCFG_AF_0);  // UART0_RX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_OUTPUT | PINCFG_AF_0);  // UART0_TX
+  conuart.baudrate = 115200;
+  conuart.Init(0);
+}
+
+#else
+  #error "Define board_pins_init here"
+#endif
+
+#ifndef HEXNUM_DISPLAY
+
+void board_show_hexnum(unsigned ahexnum)
+{
+  // nothing
+}
+
+#endif
+
--- uart/src/main.cpp
+++ uart/src/main.cpp
@@ -13,253 +13,8 @@
 #include "hwuart.h"
 #include "traces.h"
 
-THwUart   conuart;  // console uart
+#include "board_pins.h"
 
-#if defined(BOARD_VRV100_103) || defined(BOARD_VRV100_104) || defined(BOARD_VRV100_241) \
-    || defined(BOARD_VRV100_403) || defined(BOARD_VRV100_441)|| defined(BOARD_VRV100_443) || defined(BOARD_VRV100_543)
-
-TGpioPin  pin_led1(PORTNUM_A, 0, false);
-
-#define LED_COUNT 1
-
-#if SPI_SELF_FLASHING
-
-  #include "spiflash.h"
-  #include "hwspi.h"
-  #include "self_flashing.h"
-
-  THwSpi    spi;
-  TSpiFlash spiflash;
-
-#endif
-
-void show_hexnum(unsigned ahexnum)
-{
-  volatile uint32_t *  hexnum = (volatile uint32_t *)0xF1000000;
-  *hexnum = ahexnum;
-}
-
-#define HEXNUM_DISPLAY
-
-void setup_board()
-{
-	pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-	conuart.Init(1); // UART1
-
-  #if SPI_SELF_FLASHING
-
-    spi.speed = 10000000;
-    spi.Init(1); // flash
-
-    spiflash.spi = &spi;
-    spiflash.has4kerase = false; // warning some ECP devices does not have 4k erase !
-    spiflash.Init();
-
-  #endif
-}
-
-#endif
-
-#if defined(BOARD_LONGAN_NANO)
-
-TGpioPin  pin_led1(PORTNUM_C, 13, true);
-TGpioPin  pin_led2(PORTNUM_A,  1, true);
-TGpioPin  pin_led3(PORTNUM_A,  2, true);
-
-#define LED_COUNT 3
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led2.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led3.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  hwpinctrl.PinSetup(PORTNUM_A,  9, PINCFG_OUTPUT | PINCFG_AF_0);
-  hwpinctrl.PinSetup(PORTNUM_A, 10, PINCFG_INPUT  | PINCFG_AF_0);
-  conuart.Init(0); // USART0
-}
-
-#endif
-
-#if defined(BOARD_MIN_F103)
-
-TGpioPin  pin_led1(2, 13, false); // PC13
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART1
-  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_0);  // USART1_TX
-  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_0 | PINCFG_PULLUP);  // USART1_RX
-  conuart.Init(1);
-}
-#endif
-
-#if defined(BOARD_MIN_F401)
-
-TGpioPin  pin_led1(2, 13, false); // PC13
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART1
-  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART1_TX
-  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_7 | PINCFG_PULLUP);  // USART1_RX
-  conuart.Init(1);
-}
-#endif
-
-#if defined(BOARD_NUCLEO_F446) || defined(BOARD_NUCLEO_F746) || defined(BOARD_NUCLEO_H743)
-
-TGpioPin  pin_led1(PORTNUM_B, 0, false);
-TGpioPin  pin_led2(PORTNUM_B, 7, false);
-TGpioPin  pin_led3(PORTNUM_B, 14, false);
-
-#define LED_COUNT 3
-
-void setup_board()
-{
-  // nucleo board leds
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led2.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led3.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART3: Stlink USB / Serial converter
-  hwpinctrl.PinSetup(PORTNUM_D, 8,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART3_TX: PD.8
-  hwpinctrl.PinSetup(PORTNUM_D, 9,  PINCFG_INPUT  | PINCFG_AF_7);  // USART3_RX: Pd.9
-  conuart.Init(3); // USART3
-}
-
-#endif
-
-#if defined(BOARD_NUCLEO_H723)
-
-TGpioPin  pin_led1(PORTNUM_B,  0, false);  // PB0 or PA5
-TGpioPin  pin_led2(PORTNUM_E,  1, false);
-TGpioPin  pin_led3(PORTNUM_B, 14, false);
-
-#define LED_COUNT 3
-
-void setup_board()
-{
-  // nucleo board leds
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led2.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led3.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART3: Stlink USB / Serial converter
-  hwpinctrl.PinSetup(PORTNUM_D, 8,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART3_TX: PD.8
-  hwpinctrl.PinSetup(PORTNUM_D, 9,  PINCFG_INPUT  | PINCFG_AF_7);  // USART3_RX: Pd.9
-  conuart.Init(3); // USART3
-}
-
-#endif
-
-#if defined(BOARD_MIBO48_STM32G473) || defined(BOARD_MIBO48_STM32F303) || defined(BOARD_MIBO64_STM32F405)
-
-TGpioPin  pin_led1(PORTNUM_C, 13, false);
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART1
-  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART1_TX
-  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_7 | PINCFG_PULLUP);  // USART1_RX
-  conuart.Init(1);
-}
-#endif
-
-#if defined(BOARD_MIBO20_STM32F030) || defined(BOARD_MIBO20_STM32F070)
-
-TGpioPin  pin_led1(PORTNUM_B, 1, false);
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART1
-  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_1);  // USART1_TX
-  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_1 | PINCFG_PULLUP);  // USART1_RX
-  conuart.Init(1);
-}
-#endif
-
-#if defined(BOARD_XPLAINED_SAME70)
-
-TGpioPin  pin_led1(2, 8, false);  // C8
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // USART1 - EDBG
-  hwpinctrl.PinSetup(0, 21, PINCFG_INPUT | PINCFG_AF_0);  // USART1_RXD
-  MATRIX->CCFG_SYSIO |= (1 << 4); // select PB4 instead of TDI !!!!!!!!!
-  hwpinctrl.PinSetup(1,  4, PINCFG_OUTPUT | PINCFG_AF_3); // USART1_TXD
-  conuart.Init(0x101); // USART1
-
-  // UART3 - Arduino shield
-  //hwpinctrl.PinSetup(3, 28, PINCFG_INPUT | PINCFG_AF_0);  // UART3_RXD
-  //hwpinctrl.PinSetup(3, 30, PINCFG_OUTPUT | PINCFG_AF_0); // UART3_TXD
-  //uartx2.Init(3); // UART3
-}
-
-#endif
-
-#if defined(BOARD_MIBO64_ATSAME5X)
-
-TGpioPin  pin_led1(PORTNUM_A, 1, false);
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  // SERCOM0
-  hwpinctrl.PinSetup(PORTNUM_A, 4, PINCFG_OUTPUT | PINCFG_AF_3);  // PAD[0] = TX
-  hwpinctrl.PinSetup(PORTNUM_A, 5, PINCFG_INPUT  | PINCFG_AF_3);  // PAD[1] = RX
-  conuart.Init(0);
-
-  // SERCOM2
-  //hwpinctrl.PinSetup(PORTNUM_A, 12, PINCFG_AF_2);  // PAD[0] = TX
-  //hwpinctrl.PinSetup(PORTNUM_A, 13, PINCFG_AF_2);  // PAD[1] = RX
-  //conuart.Init(2);
-}
-#endif
-
-#if defined(BOARD_XPRESSO_LPC54608)
-
-TGpioPin  pin_led1(2, 2, true);
-TGpioPin  pin_led2(3, 3, true);
-TGpioPin  pin_led3(3, 14, true);
-
-#define LED_COUNT 3
-
-void setup_board()
-{
-  pin_led1.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led2.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-  pin_led3.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
-
-  hwpinctrl.PinSetup(0, 30, PINCFG_OUTPUT | PINCFG_AF_1); // UART_TX:
-  hwpinctrl.PinSetup(0, 29, PINCFG_INPUT  | PINCFG_AF_1); // UART_RX:
-  conuart.Init(0);
-}
-
-#endif
-
-#ifndef LED_COUNT
-  #define LED_COUNT 1
-#endif
-
-#ifndef HEXNUM_DISPLAY
-void show_hexnum(unsigned ahexnum)
-{
-}
-#endif
 
 volatile unsigned hbcounter = 0;
 
@@ -300,7 +55,7 @@ extern "C" __attribute__((noreturn)) void _start(unsigned self_flashing)  // sel
 	clockcnt_init();
 
 	// go on with the hardware initializations
-	setup_board();
+	board_pins_init();
 
 	TRACE("\r\n--------------------------------------\r\n");
 	TRACE("Hello From VIHAL !\r\n");
@@ -341,15 +96,12 @@ extern "C" __attribute__((noreturn)) void _start(unsigned self_flashing)  // sel
 		{
 			++hbcounter;
 
-      pin_led1.SetTo(hbcounter & 1);
-      #if LED_COUNT > 1
-        pin_led2.SetTo((hbcounter >> 1) & 1);
-      #endif
-      #if LED_COUNT > 2
-        pin_led3.SetTo((hbcounter >> 2) & 1);
-      #endif
+      for (unsigned n = 0; n < pin_led_count; ++n)
+      {
+        pin_led[n].SetTo((hbcounter >> n) & 1);
+      }
 
-      show_hexnum(hbcounter);
+      board_show_hexnum(hbcounter);
 
 			TRACE("hbcounter=%u\r\n", hbcounter);
 
