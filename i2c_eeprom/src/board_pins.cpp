cce90048e2b39a953f87b236b20f555f1419b38c
nvitya <nvitya@gmail.com>
2021-11-03 08:59:00 +0100

    working I2C with STM32F4

--- i2c_eeprom/src/board_pins.cpp
+++ i2c_eeprom/src/board_pins.cpp
@@ -113,8 +113,8 @@ void board_pins_init()
   #endif
 }
 
-#elif    defined(BOARD_MIN_F401) || defined(BOARD_MIN_F411) \
-      || defined(BOARD_MIBO64_STM32F405)
+#elif  defined(BOARD_MIN_F401) || defined(BOARD_MIN_F411) \
+      //|| defined(BOARD_MIBO64_STM32F405)
 
 void board_pins_init()
 {
@@ -134,7 +134,7 @@ void board_pins_init()
   i2c.speed = 100000; // 100 kHz
   i2c.Init(1); // I2C1
 
-  #if 0
+  #if 1
     i2c_txdma.Init(1, 6, 1);  // dma1,stream6,ch1 = I2C1 TX
     i2c_rxdma.Init(1, 5, 1);  // dma1,stream5,ch1 = I2C1 RX
 
@@ -142,6 +142,49 @@ void board_pins_init()
     i2c.DmaAssign(false, &i2c_rxdma);
   #endif
 }
+
+#elif defined(BOARD_MIBO48_STM32G473)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_C, 13, false);
+  board_pins_init_leds();
+
+
+  // USART1
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_7);  // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_7 | PINCFG_PULLUP);  // USART1_RX
+  conuart.Init(1);
+
+  // I2C1
+  // open drain mode have to be used, otherwise it won't work
+
+  // WARNING: the B6 pin did not work (was always low after enabling the AF_4)
+  //          this is due the USB-C power delivery pull down functionality
+  //          turning off did not work for me this way:
+  //  RCC->APB1ENR1 |= RCC_APB1ENR1_PWREN;
+  //  PWR->CR3 |= PWR_CR3_UCPD_DBDIS;
+  //  hwpinctrl.PinSetup(PORTNUM_B,  6, PINCFG_OUTPUT | PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SCL
+  //  hwpinctrl.PinSetup(PORTNUM_B,  7, PINCFG_OUTPUT | PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SDA
+
+  // Warning: PB8 = boot pin ! must be high at reset otherwise the Flash code does not start
+  hwpinctrl.PinSetup(PORTNUM_B,  8, PINCFG_OUTPUT | PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SCL
+  hwpinctrl.PinSetup(PORTNUM_B,  9, PINCFG_OUTPUT | PINCFG_AF_4 | PINCFG_OPENDRAIN); // I2C1_SDA
+
+  i2c.speed = 100000; // 100 kHz
+  i2c.Init(1); // I2C1
+
+  #if 1
+    i2c_txdma.Init(1, 6, 17);  // dmamux17 = I2C1 TX
+    i2c_rxdma.Init(1, 5, 16);  // dmamux16 = I2C1 RX
+
+    i2c.DmaAssign(true,  &i2c_txdma);
+    i2c.DmaAssign(false, &i2c_rxdma);
+  #endif
+
+}
+
 
 // ATSAM
 
