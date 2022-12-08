28553d2384fb208926d876620760174b2cc90a6b
nvitya <nvitya@gmail.com>
2022-12-08 17:51:42 +0100

    Wio Lite Risc-V board added

--- wifi_uart/src/board_pins.cpp
+++ wifi_uart/src/board_pins.cpp
@@ -54,6 +54,46 @@ bool TEspWifiUart::InitHw()
   return true;
 }
 
+#elif defined(BOARD_WIO_LITE_RV)
+
+void board_pins_init()
+{
+  pin_led_count = 1;
+  pin_led[0].Assign(PORTNUM_A,  8, true);
+  board_pins_init_leds();
+
+  hwpinctrl.PinSetup(PORTNUM_A,  9,  PINCFG_OUTPUT | PINCFG_AF_0);  // USART0_TX
+  hwpinctrl.PinSetup(PORTNUM_A, 10,  PINCFG_INPUT  | PINCFG_AF_0);  // USART0_RX
+  conuart.Init(0); // USART0
+}
+
+bool TEspWifiUart::InitHw()
+{
+  RCU->APB2EN |= RCU_APB2EN_AFEN;
+  uint32_t tmp = AFIO->PCF0;
+  tmp &= ~(7 << 24); // disable the NJRST
+  tmp |=  (1 << 24); // anable debug but without NJRST, because it occupies the B4
+  AFIO->PCF0 = tmp;
+
+  // ESP-01S UART WiFi Module connections
+  pin_rst.Assign(PORTNUM_B, 4, false);
+  pin_rst.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+  pin_rst.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
+  pin_rst.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+  pin_rst.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_1);
+  pin_rst.Setup(PINCFG_OUTPUT | PINCFG_GPIO_INIT_0);
+  //pin_en.Assign( PORTNUM_D, 3, false);
+
+  hwpinctrl.PinSetup(PORTNUM_A,  2, PINCFG_OUTPUT | PINCFG_AF_0); // USART1_TX
+  hwpinctrl.PinSetup(PORTNUM_A,  3, PINCFG_INPUT  | PINCFG_AF_0 | PINCFG_PULLUP); // USART1_RX
+  uart.Init(1); // USART1
+
+  dma_tx.Init(0, 6);  // dma0, ch6 = USART1_TX
+  dma_rx.Init(0, 5);  // dma0, ch5 = USART1_RX
+
+  return true;
+}
+
 //-------------------------------------------------------------------------------
 // ARM Cortex-M
 //-------------------------------------------------------------------------------
