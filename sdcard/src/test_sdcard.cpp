c2502581da9eb739e1c4a06d6909f2bc150c9e84
nvitya <nvitya@gmail.com>
2022-12-17 20:24:08 +0100

    sdcard test improvements

--- sdcard/src/test_sdcard.cpp
+++ sdcard/src/test_sdcard.cpp
@@ -17,30 +17,32 @@ void show_mem(void * addr, unsigned len)
 	TRACE("\r\n");
 }
 
-uint8_t databuf[4096];
+#define RW_BLOCK_CNT  1
+
+uint8_t databuf[4096]  __attribute__((aligned(4)));
 
 void test_sdcard()
 {
   int i;
 
-#ifdef SDCARD_SDMMC
+  #ifdef SDCARD_SDMMC
 
-	TRACE("SDCARD SDMMC test begin\r\n");
+    TRACE("SDCARD SDMMC test begin\r\n");
 
-	if (!sdcard.Init(&sd_mmc))
-	{
-	  TRACE("SDCARD init error !\r\n");
-	  return;
-	}
-#else
-  TRACE("SDCARD SPI test begin\r\n");
+    if (!sdcard.Init(&sd_mmc))
+    {
+      TRACE("SDCARD init error !\r\n");
+      return;
+    }
+  #else
+    TRACE("SDCARD SPI test begin\r\n");
 
-  if (!sdcard.Init(&sd_spi))
-  {
-    TRACE("SDCARD init error !\r\n");
-    return;
-  }
-#endif
+    if (!sdcard.Init(&sd_spi))
+    {
+      TRACE("SDCARD init error !\r\n");
+      return;
+    }
+  #endif
 
 	TRACE("Runing SDCARD state machine...\r\n");
 	while (!sdcard.card_initialized)
@@ -48,33 +50,62 @@ void test_sdcard()
 	  sdcard.Run();
 	}
 
-	// trace take time, for better scoping no traces here until the end
-
-#if 1
-
-#if 1
-
-	sdcard.StartReadBlocks(0,  &databuf[0],  1);
-	sdcard.WaitForComplete();
-
-	show_mem(&databuf[0], 512);
-
-#else
-
-	for (i = 0; i < 512*4; ++i)
-	{
-	  databuf[i] = (i >> 6);
-	}
-
-  // it is relative safe to write to the second sector
-  sdcard.StartWriteBlocks(1,  &databuf[0],  4);
-  sdcard.WaitForComplete();
-
-#endif
-
-#endif
-
   TRACE("SDCARD initialized, size = %u MB\r\n", sdcard.card_megabytes);
+
+  #if 1
+    TRACE("Writing SDCARD...\r\n");
+
+    for (i = 0; i < sizeof(databuf); ++i)
+    {
+      databuf[i] = 0xA0 + (i >> 6);
+    }
+
+    // it is relative safe to write to the second sector
+    sdcard.StartWriteBlocks(1,  &databuf[0],  RW_BLOCK_CNT);
+    sdcard.WaitForComplete();
+    if (sdcard.errorcode)
+    {
+      TRACE("  Write error: %i\r\n", sdcard.errorcode);
+    }
+    else
+    {
+      TRACE("  OK\r\n");
+    }
+  #endif
+
+
+  #if 1
+    TRACE("Reading SDCARD...\r\n");
+
+    sdcard.StartReadBlocks(1024*1024,  &databuf[0],  RW_BLOCK_CNT);
+    sdcard.WaitForComplete();
+    if (sdcard.errorcode)
+    {
+      TRACE("  Read error: %i\r\n", sdcard.errorcode);
+    }
+    else
+    {
+      TRACE("  OK\r\n");
+    }
+
+    show_mem(&databuf[0], 512 * RW_BLOCK_CNT);
+
+    TRACE("Reading SDCARD...\r\n");
+
+    sdcard.StartReadBlocks(1024*1024,  &databuf[0],  RW_BLOCK_CNT);
+    sdcard.WaitForComplete();
+    if (sdcard.errorcode)
+    {
+      TRACE("  Read error: %i\r\n", sdcard.errorcode);
+    }
+    else
+    {
+      TRACE("  OK\r\n");
+    }
+
+    show_mem(&databuf[0], 512 * RW_BLOCK_CNT);
+
+  #endif
 
 
 	TRACE("SDCARD test end\r\n");
